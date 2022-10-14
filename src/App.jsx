879f07c31c9aa9cff3cfd9b9b24a905697a7ef69
rayct <rayct@icloud.com>
2022-10-14 17:21:21 +0100

    First Commit

--- src/App.jsx
+++ src/App.jsx
@@ -22,4 +22,16 @@ function App() {
   );
 }
 
-export default App;
+// export default App;
+
+// import Table from './components/Table'
+// const App = () => {
+//   return (
+//       <div className="departures">
+//         <header>DEPARTURES</header>
+//         <Table/>
+//       </div>
+//   )
+// }
+
+// export default App
